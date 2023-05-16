d2064707b77cdfd45b549f9d240462d77ed9541f
suffaro <denys.pisotskiy@gmail.com>
2023-05-16 23:57:52 +0300

    updated repository, added all files

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<div class="footer-section footer-section1">
+<div class="footer-brand">
+    <a href="index.php">
+        <h1 class="footer-heading">Планета Кіно</h1>
+    </a> 
+</div>    
+</div>
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>© 2023 Планета Кіно. Created by O.Orlov and D.Pisotskyi. </p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a href="contact-us.php">Contact</a>
+</div>
+
